7a92aa89321884517c642801b455c0e39dd1a31f
ireferrom <ireferrom@alum.us.es>
2023-05-12 12:36:42 +0200

    taller

---
+++ sla-rate-limit/controllers/localentitiesController.js
@@ -0,0 +1,10 @@
+export function getLocalEntitiess(_, res) {
+    res.send([{ "province": "Sevilla", "president_appointment_date": "2015" }])
+}
+
+
+export function findByprovince(_, res) {
+    res.send([{ "province": "Sevilla", "president_appointment_date": "2015" }])
+}
+
+
